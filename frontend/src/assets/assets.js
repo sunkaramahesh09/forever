b94c25deed6c8776c1497d8f99b1fc901865359d
MAHESH <mahesh@MAHESHs-MacBook-Air.local>
2026-02-15 16:32:58 +0530

    changes to bestseller

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -190,7 +190,7 @@ export const products = [
         subCategory: "Topwear",
         sizes: ["S", "M", "L", "XL"],
         date: 1716622345448,
-        bestseller: false
+        bestseller: true
     },
     {
         _id: "aaaai",
